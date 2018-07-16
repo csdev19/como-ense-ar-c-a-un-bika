5e08623f484b1fb84131710f8a992327aaea940e
pystudent1913 <zcristian1913@hotmail.com>
2018-07-15 20:54:59 -0500

    ejemplo

---
+++ tarea_bika_de_mrd_3/05_ejemplo_total.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+// no retorna nada ni recibe nada
+void imprimir_problema () {
+	cout << "Bienvenido aqui haremos el siguiente problema" << endl;
+
+	cout << " (a)! / ( (a+1)! + (a+2)! ) " << endl;
+
+	cout << "a continuacion introduzca un valor " <<endl;
+
+	cout << "-> ";
+}
+
+// no retorna nada pero si recibe
+void imprimir_factorial (int a) {
+	cout << "============ El resultado es =============" << endl;
+	cout << "\t\t" << a << endln
+	cout << "==========================================" << endl;
+}
+
+
+// retorna un numero y recibe un numero
+int hallar_factorial (int a){
+	int acum =1;
+	for (int i=1; i <= a; i++) {
+		acum *= i;
+	}
+	return acum;
+}
+
+int main() {
+	int base, resultado;
+	imprimir_problema();
+	cin >> base;
+	resultado = factorial(base);
+	imprimir_factorial(resultado);
+}
